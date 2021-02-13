f895c7b9c5f23ca21c33f0b56b12d1c90f1cb17e
Himanshu Rai <hrai@anviam.com>
2021-02-13 18:15:39 +0530

    Template driven forms

--- src/app/angular-forms/template-driven-forms/template-driven-forms-index/template-driven-forms-index.component.ts
+++ src/app/angular-forms/template-driven-forms/template-driven-forms-index/template-driven-forms-index.component.ts
@@ -8,29 +8,69 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class TemplateDrivenFormsIndexComponent implements OnInit {
 
-  user = {
-    firstName: '',
-    lastName: '',
-    email: '',
-    password: '',
-    confirmPassword: '',
-    temsCondition: '',
-    color: ''
-  };
+
   // {
-  //   "name": {
-  //     "firstName": "",
-  //     "lastName": ""
+  //   "input": {
+  //     "input_text": "",
+  //     "input_email": "rarora+1@anviam.com",
+  //     "input_password": "BH84udf0",
+  //     "input_color": "",
+  //     "input_date": "",
+  //     "input_datetime-local": "",
+  //     "input_file": "",
+  //     "input_month": "",
+  //     "input_number": "",
+  //     "input_range": "",
+  //     "input_reset": "",
+  //     "input_search": "",
+  //     "input_tel": "",
+  //     "input_time": "",
+  //     "input_url": "",
+  //     "input_week": "",
+  //     "input_checkbox": ""
   //   },
-  //   "email": "",
-  //   "password": "",
-  //   "confirmPassword": "",
-  //   "temsCondition": ""
+  //   "gender": {
+  //     "male": "",
+  //     "female": ""
+  //   }
   // }
 
+  user;
+  showInitialized: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
+
+  }
+
+  initializeForm() {
+    this.showInitialized = true;
+    this.user = {
+      "input": {
+        "input_text": "Test",
+        "input_email": "test@test.com",
+        "input_password": "BH84udf0",
+        "input_color": "#595496",
+        "input_date": "2021-02-13",
+        "input_datetime_local": "2021-02-13T16:01",
+        "input_file": "C:\\fakepath\\ROR.odt",
+        "input_month": "2021-02",
+        "input_number": 1234567890,
+        "input_range": 29,
+        "input_reset": "",
+        "input_search": "12345abc",
+        "input_tel": "1234567890",
+        "input_time": "16:03",
+        "input_url": "http://localhost:8000/angular-forms/template-driven-forms",
+        "input_week": "2021-W06",
+        "input_checkbox": ""
+      },
+      "gender": {
+        "male": "true",
+        "female": "false"
+      }
+
+    };
   }
 
   onSubmitForm(data) {
